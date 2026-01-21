fb00c5601a30ed7b38bb9d37ea4d9d9c77fad605
Poonam9075 <khandagalepoonam04@gmail.com>
2026-01-21 22:33:08 +0530

    Added Changes Glamify Services

---
+++ src/main/java/com/Glamify/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.Glamify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Glamify.dto.AuthRequest;
+import com.Glamify.dto.AuthResponse;
+import com.Glamify.services.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody @Valid AuthRequest request) {
+
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
