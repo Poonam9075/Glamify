6a587d6050645ff798c69f912fe675d8878fc4df
Poonam Khandagale <130628612+Poonam9075@users.noreply.github.com>
2026-02-09 00:10:37 +0530

    Update OtpController.java

--- src/main/java/com/Glamify/controller/OtpController.java
+++ src/main/java/com/Glamify/controller/OtpController.java
@@ -15,41 +15,6 @@ import com.Glamify.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.Glamify.dto.ApiResponse;
-//import com.Glamify.dto.OtpVerifyDTO;
-//import com.Glamify.services.OtpService;
-//import com.Glamify.services.UserService;
-//
-//import lombok.RequiredArgsConstructor;
-//
-//@RestController
-//@RequestMapping("/otp")
-//@RequiredArgsConstructor
-//public class OtpController {
-//
-//    private final OtpService otpService;
-//    private final UserService userService;
-//
-//    @PostMapping("/verify")
-//    public ResponseEntity<ApiResponse> verifyOtp(
-//            @RequestBody OtpVerifyDTO dto) {
-//
-//        otpService.verifyOtp(dto.getEmail(), dto.getOtp());
-//
-//        userService.activateUser(dto.getEmail());
-//
-//        return ResponseEntity.ok(
-//                new ApiResponse("SUCCESS", "Account verified successfully")
-//        );
-//    }
-//}
 
 @RestController
 @RequestMapping("/auth/otp")
@@ -58,10 +23,6 @@ public class OtpController {
 
     private final OtpService otpService;
     private final UserService userService;
-
-  
-
-
 
 @PostMapping("/verify")
 public ResponseEntity<ApiResponse> verifyOtp(@Valid @RequestBody OtpVerifyDTO request) {
