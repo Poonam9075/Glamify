21382d42c3f39a722707a56ab37276e88039dbfc
Poonam Khandagale <130628612+Poonam9075@users.noreply.github.com>
2026-02-09 00:17:48 +0530

    Update User.java

--- src/main/java/com/Glamify/entities/User.java
+++ src/main/java/com/Glamify/entities/User.java
@@ -13,11 +13,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity{
-
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long user_id;
-
+	
 	@Column(name = "first_name", length=40,nullable = false)
     private String firstName;
 	@Column(name = "last_name", length=40,nullable = false)
